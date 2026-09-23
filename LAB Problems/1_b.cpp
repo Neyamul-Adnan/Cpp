77f5718f336cfd79a8b50180bede3169d5a0e7fc
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-09-23 22:32:14 +0600

    initial

--- LAB Problems/1_b.cpp
+++ LAB Problems/1_b.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    
-    return 0;
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    cout << "Enter array elements: ";
+    int data[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> data[i];
+    }
+
+    cout << "Now enter elements to search: ";
+    int item;
+    cin >> item;
+
+    bool found = false;
+    int location;
+
+    int low = 0;
+    int high = n - 1;
+    int mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        cout << "  Low: " << low;
+        cout << "  High: " << high;
+        cout << "  Mid: " << mid << endl;
+
+        if (item == data[mid])
+        {
+            found = true;
+            location = mid;
+            break;
+        }
+        else if (item < data[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if (found == true){
+        cout << "Item found at location: " << location;
+}
+else
+{
+    cout << "Item not found";
+}
+return 0;
 }
