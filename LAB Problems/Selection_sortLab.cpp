3e8b6d7cfe8f6311b4cb033bd9c7933beb4e0863
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-09-25 12:20:31 +0600

    Improve code formatting

--- LAB Problems/Selection_sortLab.cpp
+++ LAB Problems/Selection_sortLab.cpp
@@ -5,43 +5,44 @@ using namespace std;
 int main()
 {
 
-    int n=5;
+    int n = 5;
     int element[n], temp;
     srand(time(0));
 
-    
-    cout<<"Enter the number of elements you want to sort:";
-    cin>>n;
+    cout << "Enter the number of elements you want to sort:";
+    cin >> n;
 
-    for(int i=0; i<n; i++){
-        element[i]=rand();
+    for (int i = 0; i < n; i++)
+    {
+        element[i] = rand();
     }
-    cout<<"The random elements are: "<<endl;
-    for(int i=0; i<n; i++){
-        cout<<" "<<element[i];
+    cout << "The random elements are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << " " << element[i];
     }
-    cout<<endl;
+    cout << endl;
 
-    for(int i=0; i<n-1; i++){
-        int min=i;
-        for(int j=i+1; j<n; j++){
-            if(element[j]<element[min]){
-                min=j;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (element[j] < element[min])
+            {
+                min = j;
             }
         }
         temp = element[min];
-        element[min]=element[i];
-        element[i]=temp;
-
+        element[min] = element[i];
+        element[i] = temp;
     }
 
-    cout<<"After Selection sorting the elements are:"<<endl;
-    for(int i=0; i<n; i++){
-        cout<<" "<<element[i];
+    cout << "After Selection sorting the elements are:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << " " << element[i];
     }
-
-
 
     return 0;
-
 }
